1d5a8d20a192c18402ae909e2cbad7550f435239
eden-yl <lhs754579@gmail.com>
2024-02-29 13:15:39 +0900

    edit-20240229

--- src/components/NotionLike.vue
+++ src/components/NotionLike.vue
@@ -27,6 +27,16 @@
               </v-card>
             </v-col>
           </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-card>
+                <v-card-title>Page 3</v-card-title>
+                <v-card-text>
+                  <p>This is the third page in the Notion-like design.</p>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-container>
       </v-main>
     </v-app>
