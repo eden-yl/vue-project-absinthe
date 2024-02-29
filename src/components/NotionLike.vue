68bcd9a18b5f468a10c6b8695973c2e51babd43a
eden <161665058+eden-yl@users.noreply.github.com>
2024-02-29 11:05:37 +0900

    Add files via upload

---
+++ src/components/NotionLike.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-app>
+      <v-main>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <h1>Notion-like Design</h1>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-card>
+                <v-card-title>Page 1</v-card-title>
+                <v-card-text>
+                  <p>This is a sample page in the Notion-like design.</p>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-card>
+                <v-card-title>Page 2</v-card-title>
+                <v-card-text>
+                  <p>This is another sample page in the Notion-like design.!!</p>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NotionLike',
+  }
+  </script>
